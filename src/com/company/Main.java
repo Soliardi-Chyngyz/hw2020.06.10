3f1c13795d973be50b56b64b90f0d93d139e5fc6
Soliardi-Chyngyz <wxwaren@gmail.com>
2020-06-11 12:44:39 +0600

    Second upload

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,26 +12,29 @@ public class Main {
         String name = dataBase.nextLine();
         int age = dataBase.nextInt();
         int temperature = dataBase.nextInt(); !!я бы хотел выводить на консоль ввод данных и результат, не подскажите?*/
-        basa(23, 30);
-        basa(10,44);
-        basa(20,20);
-        basa(30,60);
-        basa(14,15);
+        boolean b = basa(23, 30);
+        boolean c = basa(10,44);
+        boolean d = basa(20,20);
+        boolean e = basa(30,60);
+        boolean f = basa(14,15);
 
-        generateRandomAge();
+        int a = generateRandomAge();
 
     }
 
-    private static void basa(/*String name какой тип вместо void я должен прописать для строчных и целых чисел?*/int age, int temperature) {
-
+    public static boolean basa(/*String name какой тип вместо void я должен прописать для строчных и целых чисел?*/int age, int temperature) {
         if (age >= 20  && age <= 45 && temperature >= -20 && temperature <= 30){
             System.out.println("You be allowed to walk away");
+            return true;
         } else if (age <= 20 && temperature >=0 && temperature <= 28){
             System.out.println("You be allowed to walk away");
+            return true;
         } else if(age >= 45 && temperature >= -10 && temperature <= 25){
             System.out.println("You be allowed to walk away");
+            return true;
         } else {
             System.out.println("Stay at home!");
+            return false;
         }
     }
     public static int generateRandomAge(){
