468f7f0e74a9eb3ecaf7f7a0c669241b19798209
Soliardi-Chyngyz <wxwaren@gmail.com>
2020-08-17 17:41:58 +0600

    Final upload

--- src/com/company/Permissive.java
+++ src/com/company/Permissive.java
@@ -1,49 +1,46 @@
 package com.company;
 
 import java.awt.*;
+import java.sql.SQLOutput;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Permissive {
 
     public static void main(String[] args) {
-        /*Scanner dataBase = new Scanner(System.in);
-        System.out.println("Вводите через Enter ваше имя, Возраст, Температуру воздуха:");
-        String name = dataBase.nextLine();
-        int age = dataBase.nextInt();
-        int temperature = dataBase.nextInt(); !!я бы хотел выводить на консоль ввод данных и результат, не подскажите?*/
-        System.out.println(basa(23,30));
-        System.out.println(basa(10,28));
-        System.out.println(basa(30,50));
-        System.out.println(basa(50,20));
-        System.out.println(basa(18,0));
-
-        int a = generateRandomAge();
+//        Scanner dataBase = new Scanner(System.in);
+//        System.out.println("Вводите через Enter ваше имя, Возраст, Температуру воздуха:");
+//        String name = dataBase.nextLine();
+//        int age = dataBase.nextInt();
+//        int temperature = dataBase.nextInt();
+//        System.out.println("Name: " +  name + ", "+ "Age: " + age + ", " + "Temperature: " + temperature);
+        basa("Vasa", generateRandomAge(), 24);
 
     }
 
-    public static String basa(/*String name какой тип вместо void я должен прописать для строчных и целых чисел?*/int age, int temperature) {
-        boolean permision = true;
 
-
+    public static String basa(String name, int age, int temperature) {
         if (age >= 20  && age <= 45 && temperature >= -20 && temperature <= 30){
-            return "You be allowed to walk away";
+            System.out.println("Name: " +  name + ", "+ "Age: " + age + ", " + "Temperature: " + temperature);
+            return "You be allowed to walk away\n";
         } else if (age <= 20 && temperature >=0 && temperature <= 28){
-            return "You be allowed to walk away";
+            System.out.println("Name: " +  name + ", "+ "Age: " + age + ", " + "Temperature: " + temperature);
+            return "You be allowed to walk away\n";
         } else if(age >= 45 && temperature >= -10 && temperature <= 25){
-            return "You be allowed to walk away";
+            System.out.println("Name: " +  name + ", "+ "Age: " + age + ", " + "Temperature: " + temperature);
+            return "You be allowed to walk away\n";
         } else {
-            return "Stay at home!";
+            System.out.println("Name: " +  name + ", "+ "Age: " + age + ", " + "Temperature: " + temperature);
+            return "Stay at home!\n";
         }
     }
     public static int generateRandomAge(){
         int a = 1+(int)(Math.random()*101);
         if (a<16 || a>70){
-            System.out.println("You couldn't leave a home");
+            System.out.println("You couldn't leave home");
         } else {
-            System.out.println("Yeah, you be able to leave home");
+            System.out.println("Yeah!, you be able to leave home");
         }
         return a;
-//        у меня вопрос как просмотреть какое число сгенерировал этот метод generateRandomAge?
     }
 }
